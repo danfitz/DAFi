9c8880c0e87fe650d910c494413af57139050da7
Dan Fitz <8danfitz8@gmail.com>
2018-03-21 13:55:50 -0400

    add Goal and User models + flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,31 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from config import Config
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = "auth.login"
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     bootstrap.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
     return app
+
+from app import models
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,17 @@
 from app.main import bp
-from flask import render_template
+from flask import render_template, current_app
+from flask_login import login_required
+
+import os
+from flask import send_from_directory
 
 @bp.route("/")
+@login_required
 def index():
     return render_template("index.html")
+
+# view function necessary for favicon to appear
+@bp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(current_app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
