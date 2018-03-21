52f99040d8ebf2a2ef90f89dd1cbcdda6400ddbd
Dan Fitz <8danfitz8@gmail.com>
2018-03-21 14:50:04 -0400

    add parent-child rel for goals in db

--- app/models.py
+++ app/models.py
@@ -24,6 +24,11 @@ class User(UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
+parents = db.Table("parents",
+    db.Column("parent_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("child_id", db.Integer, db.ForeignKey("user.id"))
+)
+
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     goal = db.Column(db.String(100))
@@ -32,6 +37,37 @@ class Goal(db.Model):
     rating = db.Column(db.Integer)
     total_rating = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    children = db.relationship(
+        "Goal", secondary=parents,
+        primaryjoin=(parents.c.parent_id == id),
+        secondaryjoin=(parents.c.child_id == id),
+        backref=db.backref("parents", lazy="dynamic"),
+        lazy="dynamic"
+    )
 
     def __repr__(self):
         return "<Goal: {}>".format(self.goal)
+
+    def is_parent(self, goal):
+        return self.children.filter(parents.c.child_id == goal.id).count() > 0
+
+    def is_child(self, goal):
+        return self.parents.filter(parents.c.parent_id == goal.id).count() > 0
+
+    def add_child(self, goal):
+        if not (self.is_parent(goal) or self.is_child(goal)):
+            self.children.append(goal)
+        else:
+            return "'{}' is already connected to '{}'".format(self.goal, goal.goal)
+
+    def add_parent(self, goal):
+        if not (self.is_child(goal) or self.is_parent(goal)):
+            self.parents.append(goal)
+        else:
+            return "'{}' is already connected to '{}'".format(self.goal, goal.goal)
+
+    def remove_rel(self, goal):
+        if self.is_parent(goal):
+            self.children.remove(goal)
+        elif self.is_child(goal):
+            self.parents.remove(goal)
